6c4d8f4c8ab72cb4da9434d2fc809eae2b5d3723
Aram Dawara <arram.dawara@gma>
2024-09-23 10:21:25 +0200

    .

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ document.querySelectorAll('.faq-question').forEach(question => {
     });
 });
 
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-});
-document.addEventListener('keydown', function(e) {
-    if (e.ctrlKey && (e.key === 'u' || e.key === 's')) {
-        e.preventDefault();
-    }
-});
