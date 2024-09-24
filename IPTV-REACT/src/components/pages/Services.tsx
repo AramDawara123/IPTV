1ab17ec192fc85a1ab16f52cb37c70a0d405d4fd
Adil Harhour <adilharhour@gmail.com>
2024-09-24 11:06:35 +0200

    índex.html

---
+++ IPTV-REACT/src/components/pages/Services.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Services: React.FC = () => {
+  return (
+    <>
+      <h1>Services</h1>
+      <div></div>
+      <br />
+      <div></div>
+      <br />
+      <div></div>
+      <br />
+      <div></div>
+      <br />
+      <div></div>
+      <br />
+      <div></div>
+      <br />
+      <div></div>
+    </>
+  );
+};
