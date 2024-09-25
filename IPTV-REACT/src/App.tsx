f2b298c424c7c7e59520537cb2943f951c577646
Adil Harhour <adilharhour@gmail.com>
2024-09-25 11:23:47 +0200

    índex.html

--- IPTV-REACT/src/App.tsx
+++ IPTV-REACT/src/App.tsx
@@ -14,8 +14,6 @@ const App: React.FC = () => {
         <Route path="/contact" element={<Contact />} />
       </Routes>
         <div>
-          <h1>e</h1>
-
         </div> 
     </div>
     
--- IPTV-REACT/src/components/Navbar.tsx
+++ IPTV-REACT/src/components/Navbar.tsx
@@ -8,19 +8,21 @@ export const Navbar: React.FC = () => {
   return (
     <nav>
       <Link to="/" className="title">
-        Website
+        Streamium
       </Link>
+
       <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
         <span></span>
         <span></span>
         <span></span>
       </div>
+
       <ul className={menuOpen ? "open" : ""}>
         <li>
-          <NavLink to="/about">About</NavLink>
+        <NavLink to="/services">Services</NavLink>
         </li>
         <li>
-          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/about">About</NavLink>
         </li>
         <li>
           <NavLink to="/contact">Contact</NavLink>
