24a1ed5676f60b2554cb21721746d29685c52d04
Tiesvo <ties@vanoldenborgh.com>
2024-09-25 20:52:26 +0200

    api data endpoint

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify
-
+import json
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'hallo'
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    data = {"Info": "hier komt de info van de api"}
+    with open('data.json', 'r') as file:
+        data = json.load(file)
     return jsonify(data)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5011, debug=True)
