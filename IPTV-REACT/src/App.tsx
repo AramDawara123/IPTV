40bd28e0ce6ae637147e9bf140da4d2de588c34a
Akshay12184 <144685658+Akshay12184@users.noreply.github.com>
2024-09-25 12:13:22 +0200

    working on service.tsx

--- IPTV-REACT/src/App.tsx
+++ IPTV-REACT/src/App.tsx
@@ -13,18 +13,13 @@ const App: React.FC = () => {
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-<<<<<<< HEAD
-        <div>
-        </div> 
-=======
       
       <div className="informatica">
-      <h1>All-In-One Premium IPTV Service</h1>
+      <h3>All-In-One Premium IPTV Service</h3>
             <p>Enjoy your time with excellent image quality up to 4K, on any device, anytime and anywhere,with over
                 +18,000 channels, +88,000 VOD and uptime 100%</p>
             <button className="free-button"><a href="https://wa.me/qr/2WXRBEQMIZKEB1">Free Trial</a></button>
       </div>
->>>>>>> fd8f091e3dfda798b0c80a1652f0f2085a696d93
     </div>
 
   );
--- IPTV-REACT/src/components/pages/Services.tsx
+++ IPTV-REACT/src/components/pages/Services.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 
+
+const getData = () => {
+  fetch('data.json')
+}
+
+console.log(getData);
+
+
 export const Services: React.FC = () => {
   return (
     <>
       <h1>Services</h1>
-        <div>
-        <p>we have movies, shows and series that we provide these are some examples you will get whne you buy our service</p>
+        <div className="intropage-service">
+        <p>we have movies, shows and series that we provide you can search any movie, serie or show</p>
         </div>
       <br/>
-      <div></div>
-      <br />
-      <div></div>
-      <br />
-      <div></div>
-      <br />
-      <div></div>
-      <br />
-      <div></div>
-      <br />
-      <div></div>
+      <div className="example">
+        <p></p>
+      </div>
+      <br/>
     </>
   );
 };
