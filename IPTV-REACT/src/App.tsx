fd8f091e3dfda798b0c80a1652f0f2085a696d93
Akshay12184 <144685658+Akshay12184@users.noreply.github.com>
2024-09-25 10:49:19 +0200

    commited small changes for app.tsx

--- IPTV-REACT/src/App.tsx
+++ IPTV-REACT/src/App.tsx
@@ -13,14 +13,13 @@ const App: React.FC = () => {
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-
+      
       <div className="informatica">
       <h1>All-In-One Premium IPTV Service</h1>
             <p>Enjoy your time with excellent image quality up to 4K, on any device, anytime and anywhere,with over
                 +18,000 channels, +88,000 VOD and uptime 100%</p>
             <button className="free-button"><a href="https://wa.me/qr/2WXRBEQMIZKEB1">Free Trial</a></button>
       </div>
-      
     </div>
 
   );
--- IPTV-REACT/src/components/pages/Services.tsx
+++ IPTV-REACT/src/components/pages/Services.tsx
@@ -4,7 +4,9 @@ export const Services: React.FC = () => {
   return (
     <>
       <h1>Services</h1>
-      <div></div>
+        <div>
+        <p>we have movies, shows and series that we provide these are some examples you will get whne you buy our service</p>
+        </div>
       <br/>
       <div></div>
       <br />
