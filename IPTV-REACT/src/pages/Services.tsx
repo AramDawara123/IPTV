394708eef02275692a274a769af9d0454f0575f6
Akshay12184 <144685658+Akshay12184@users.noreply.github.com>
2024-09-26 12:04:01 +0200

    made an functioning banner. working on styling it

--- IPTV-REACT/src/pages/Services.tsx
+++ IPTV-REACT/src/pages/Services.tsx
@@ -1,33 +1,55 @@
-import React from "react";
-// import { Suspense } from "react";
+import React, { useState, useEffect } from "react";
 import "../CSS/Service.css";
 
-const getData = async () => {
-  try {
-    const response = await fetch('/mock.json');
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
-
-
-
-export const Services: React.FC = () => {
+const Card = ({ movies }) => {
   return (
-    <>
-      <h1>Services</h1>
-        <div className="intropage-service">
-        <p>we have movies, shows and series that we provide you can search any movie, serie or show</p>
+    <div className="cardlist__movies">
+      {movies.map((movie, index) => (
+        <div className="card" key={index}>
+          <img className="movie__image" src={movie.cover} alt={movie.name} />
+          <div className="flex__card">
+            <p className="heading">{movie.name}</p>
+            <p className="paragraph">{movie.plot}</p>
+            <p className="paragraph">Cast: {movie.cast}</p>
+            <p className="paragraph">Release Date: {movie.releaseDate}</p>
+            <p className="paragraph">Rating: {movie.rating} / 10</p>
+          </div>
         </div>
-      <br/>
-      <div className="example">
-        <p></p>
-      </div>
-      <br/>
-    </>
+      ))}
+    </div>
   );
 };
 
-getData();
+export const Services: React.FC = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/mock.json"); 
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>Services</h1>
+      <div className="intropage-service">
+        <p>
+          We have movies, shows, and series that we provide. You can search any
+          movie, series, or show.
+        </p>
+      </div>
+      <br />
+      {/* Render the Card component and pass the fetched movies data */}
+      {movies.length > 0 ? <Card movies={movies} /> : <p>Loading...</p>}
+    </>
+  );
+};
