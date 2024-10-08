c00c1ff2b7448edfab9c7a37a5aac92c1e040ab0
Adil Harhour <adilharhour@gmail.com>
2024-10-08 14:58:08 +0200

    kleine video.tsx changes

---
+++ IPTV-REACT/src/pages/Video.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../CSS/Video.css";
+
+const TrailerComponent: React.FC = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default TrailerComponent;
